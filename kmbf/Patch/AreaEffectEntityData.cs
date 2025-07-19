32a93bda439d68e7336080464cb0e2191e0b3759
kaboissonneault <kaboissonneault@gmail.com>
2025-07-19 11:18:36 -0400

    Added "Area of Effect" UnitEnter/Round double trigger fix from TabletopTweaks

---
+++ kmbf/Patch/AreaEffectEntityData.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using Kingmaker.UnitLogic.Abilities.Components.AreaEffects;
+using Kingmaker.UnitLogic.Mechanics;
+using Kingmaker.Utility;
+using Kingmaker.View.MapObjects;
+
+namespace kmbf.Patch
+{
+    [HarmonyPatch(typeof(AreaEffectEntityData), MethodType.Constructor, new Type[]{
+            typeof(AreaEffectView),
+            typeof(MechanicsContext),
+            typeof(BlueprintAbilityAreaEffect),
+            typeof(TargetWrapper),
+            typeof(TimeSpan),
+            typeof(TimeSpan?),
+            typeof(bool),
+        })]
+    static class AreaOfEffectsTick_Round_Patch
+    {
+        // Prevents Area of Effect spells with both a UnitEnter event and a round event from triggering twice
+        [HarmonyPostfix]
+        public static void Postfix(AreaEffectEntityData __instance)
+        {
+            if (!Main.UMMSettings.BalanceSettings.FixAreaOfEffectDoubleTrigger) { return; }
+            if (__instance.Blueprint.GetComponent<AbilityAreaEffectRunAction>()?.UnitEnter?.HasActions ?? false)
+            {
+                __instance.m_TimeToNextRound = 6f;
+            }
+        }
+    }
+}
