574a1cbb3bb119f184bf85fd4b54d8bdf341eb1f
kaboissonneault <kaboissonneault@gmail.com>
2025-07-24 19:07:25 -0400

    Patch Vital Strike to allow ranged attacks

---
+++ kmbf/Patch/VitalStrike.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using Kingmaker.Controllers.Combat;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.Items.Slots;
+using Kingmaker.UnitLogic.Abilities.Components;
+using Kingmaker.Utility;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace kmbf.Patch
+{
+    // UnitEngagementExtension.GetThreatHand does not work with Ranged weapon, and therefore fails to resolve AbilityCustomMeleeAttack.Deliver for Vital Strike,
+    // which should work on ranged weapons
+    // Patch in a custom function that simply returns the primary hand weapon for Vital Strike
+    // Taken from Call of the Wild
+    [HarmonyPatch(typeof(AbilityCustomMeleeAttack), nameof(AbilityCustomMeleeAttack.Deliver), MethodType.Enumerator)]
+    static class AbilityCustomMeleeAttack_Deliver_Patch
+    {
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> Deliver_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            MethodInfo UnitEngagementExtension_GetThreatHand = AccessTools.Method(typeof(UnitEngagementExtension), nameof(UnitEngagementExtension.GetThreatHand));
+            MethodInfo Patch_GetWeaponSlot = AccessTools.Method(typeof(AbilityCustomMeleeAttack_Deliver_Patch), nameof(GetWeaponSlot));
+
+            foreach(CodeInstruction instruction in instructions)
+            {
+                if (!instruction.Calls(UnitEngagementExtension_GetThreatHand))
+                {
+                    yield return instruction;
+                    continue;
+                }
+                else
+                {
+                    // Load the AbilityCustomMeleeAttack, then call GetWeaponSlot
+                    yield return new CodeInstruction(OpCodes.Ldloc_1, null);
+                    yield return new CodeInstruction(OpCodes.Call, Patch_GetWeaponSlot);
+                }
+            }
+        }
+
+        static WeaponSlot GetWeaponSlot(UnitEntityData attacker, AbilityCustomMeleeAttack customAbility)
+        {
+            return !customAbility.IsVitalStrike ? attacker.GetThreatHand() : attacker.Body.PrimaryHand;
+        }
+    }
+}
