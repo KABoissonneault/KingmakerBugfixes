5fb69c5102edf2bc4a3988da2ef79fc8d477ffab
kaboissonneault <kaboissonneault@gmail.com>
2025-07-24 14:03:08 -0400

    Added patch from Wrath Patches to properly dispose Initiative Tracker unit buffs on updates

---
+++ kmbf/Patch/EventSubscription.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Kingmaker.UI._ConsoleUI.TurnBasedMode;
+using Kingmaker.Utility;
+
+namespace kmbf.Patch
+{
+    [HarmonyPatch]
+    static class EventSubscription
+    {
+        // UnitBuffs in the initiative tracker are cleared and re-created every update, but not disposed
+        [HarmonyPatch(typeof(InitiativeTrackerUnitVM), nameof(InitiativeTrackerUnitVM.UpdateBuffs))]
+        [HarmonyPrefix]
+        static void PatchUpdateBuffs(InitiativeTrackerUnitVM __instance) => __instance.UnitBuffs.ForEach(x => x.Dispose());
+    }
+}
