1a0e87c57e3e6644011e547eac8c41b7b86f3b67
kaboissonneault <kaboissonneault@gmail.com>
2025-07-25 13:00:53 -0400

    Fixed exception throw at launch due to missing null check in BugReportCanvas.OnEnable

---
+++ kmbf/Patch/KingmakerUI.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using Kingmaker.UI;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace kmbf.Patch
+{
+    [HarmonyPatch(typeof(BugReportCanvas), nameof(BugReportCanvas.OnEnable))]
+    static class BugReportCanvas_OnEnable_Transpiler
+    {
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> OnEnable_Transpile(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            MethodInfo objectEquals = AccessTools.Method(typeof(UnityEngine.Object), nameof(UnityEngine.Object.Equals));
+            FieldInfo bugReportButtonGet = AccessTools.Field(typeof(BugReportCanvas), nameof(BugReportCanvas.BugReportButton));
+
+            List<CodeInstruction> newInstructions = new List<CodeInstruction>();
+
+            foreach(CodeInstruction instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Ldfld && instruction.operand.Equals(bugReportButtonGet))
+                {
+                    var jumpTarget = new CodeInstruction(OpCodes.Nop, null);
+                    var jumpLabel = generator.DefineLabel();
+                    jumpTarget.labels.Add(jumpLabel);
+
+                    newInstructions.Add(instruction);
+                    newInstructions.Add(new CodeInstruction(OpCodes.Ldnull));
+                    newInstructions.Add(new CodeInstruction(OpCodes.Call, objectEquals));
+                    newInstructions.Add(new CodeInstruction(OpCodes.Brfalse_S, jumpLabel));
+                    newInstructions.Add(new CodeInstruction(OpCodes.Ret));
+                    newInstructions.Add(jumpTarget);
+                    newInstructions.Add(new CodeInstruction(OpCodes.Ldarg_0, null));
+                    newInstructions.Add(new CodeInstruction(OpCodes.Ldfld, instruction.operand));
+                }
+                else
+                {
+                    newInstructions.Add(instruction);
+                }
+            }
+
+            return newInstructions;
+        }
+    }
+}
