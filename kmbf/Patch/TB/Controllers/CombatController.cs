d091a36e749305ac35f6a7ee5d5c4367ad5bf223
kaboissonneault <kaboissonneault@gmail.com>
2025-07-31 16:00:16 -0400

    Fixed turn-based combat always starting a surprise round if combat is started in the first 6 seconds of a minute, as well as sometimes removing the action of the protagonist instead of the unit that actually did a surprise action

---
+++ kmbf/Patch/TB/Controllers/CombatController.cs
@@ -0,0 +1,62 @@
+﻿using HarmonyLib;
+using Kingmaker;
+using Kingmaker.Controllers;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UnitLogic.Abilities;
+using Kingmaker.Utility;
+using System.Reflection;
+using System.Reflection.Emit;
+using TurnBased.Controllers;
+using TurnBased.Utility;
+
+namespace kmbf.Patch.TB.Controllers
+{
+    [HarmonyPatch]
+    static class CombatController_HandleCombatStart_Postfix
+    {
+        static readonly MethodInfo timeSpanSeconds = AccessTools.PropertyGetter(typeof(TimeSpan), nameof(TimeSpan.Seconds));
+        static readonly MethodInfo timeSpanTotalSeconds = AccessTools.PropertyGetter(typeof(TimeSpan), nameof(TimeSpan.TotalSeconds));
+
+        // The code checks if the timespan between now and the last surprise action is less than 6 seconds ago
+        // If the current time is 560.12:49:03 and the protagonist's last surprise action is 00:00:00 (never this run),
+        // the difference has a Seconds of 3, and a TotalSeconds of 48,430,143
+        // 3 is less than 6, so the protagonist gets the surprise action
+        // This is obviously wrong
+        [HarmonyPatch(typeof(CombatController), nameof(CombatController.HandleCombatStart))]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> HandleCombatStart_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return DoPatch(instructions);
+        }
+
+        [HarmonyPatch("TurnBased.Controllers.CombatController+<>c__DisplayClass79_0", "<HandleCombatStart>b__3")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> HandleCombatStart_Lambda_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return DoPatch(instructions);
+        }
+
+        static IEnumerable<CodeInstruction> DoPatch(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> newInstructions = new List<CodeInstruction>();
+
+            foreach (var instruction in instructions)
+            {
+                if (instruction.Calls(timeSpanSeconds))
+                {
+                    var newInstruction = new CodeInstruction(OpCodes.Call, timeSpanTotalSeconds);
+                    newInstruction.labels = instruction.labels;
+                    newInstruction.blocks = instruction.blocks;
+                    newInstructions.Add(newInstruction);
+                }
+                else
+                {
+                    newInstructions.Add(instruction);
+                }
+            }
+
+            return newInstructions;
+        }
+    }
+}
