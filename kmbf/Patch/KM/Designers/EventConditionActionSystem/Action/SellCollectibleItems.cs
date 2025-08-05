be87976d9ea7c257aabd0f24653725cacdfdcf32
kaboissonneault <kaboissonneault@gmail.com>
2025-08-05 12:04:43 -0400

    Fixed issue where Storyteller would grab relics one at a time if stacked with mods (ex: Inventory Tweaks)

---
+++ kmbf/Patch/KM/Designers/EventConditionActionSystem/Action/SellCollectibleItems.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using Kingmaker;
+using Kingmaker.Designers;
+using Kingmaker.Designers.EventConditionActionSystem.Actions;
+
+namespace kmbf.Patch.KM.Designers.EventConditionActionSystem.Action
+{
+    [HarmonyPatch(typeof(SellCollectibleItems), nameof(SellCollectibleItems.RunAction))]
+    static class SellCollectibleItems_RunAction_Prefix
+    {
+        [HarmonyPrefix]
+        static bool RunAction(SellCollectibleItems __instance)
+        {
+            int itemCost = ((!__instance.HalfPrice) ? __instance.ItemToSell.Cost : (__instance.ItemToSell.Cost / 2));
+            int itemCount = GameHelper.GetPlayerCharacter().Inventory.Filter(__instance.ItemToSell).Sum(i => i.Count);
+            if (itemCount > 0)
+            {
+                GameHelper.GetPlayerCharacter().Inventory.Remove(__instance.ItemToSell, itemCount);
+                GameHelper.GetPlayerCharacter().Inventory.Add(Game.Instance.BlueprintRoot.CoinItem, itemCount * itemCost);
+            }
+
+            return false;
+        }
+    }
+}
