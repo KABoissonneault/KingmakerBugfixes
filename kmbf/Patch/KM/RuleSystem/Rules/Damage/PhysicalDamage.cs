c8cafb3cbe137f878250bbb40f0c9053f06e8722
kaboissonneault <kaboissonneault@gmail.com>
2025-08-10 20:27:55 -0400

    Fixed issue where sneak/precision damage could fail to bypass DR that the weapon should be able to bypass. For example, attacking a target with Protection from Arrows with a Longbow +1 would properly avoid the DR for the main damage, but would still be applied to the sneak damage

---
+++ kmbf/Patch/KM/RuleSystem/Rules/Damage/PhysicalDamage.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Kingmaker.RuleSystem.Rules.Damage;
+
+namespace kmbf.Patch.KM.RuleSystem.Rules.Damage
+{
+    [HarmonyPatch(typeof(PhysicalDamage), nameof(PhysicalDamage.CreateTypeDescription))]
+    internal class PhysicalDamage_CreateTypeDescription_Postfix
+    {
+        [HarmonyPostfix]
+        static void CreateTypeDescriptionPostfix(ref DamageTypeDescription __result, PhysicalDamage __instance)
+        {
+            __result.Physical.Enhancement = __instance.Enchantment;
+            __result.Physical.EnhancementTotal = __instance.EnchantmentTotal;
+        }
+    }
+}
