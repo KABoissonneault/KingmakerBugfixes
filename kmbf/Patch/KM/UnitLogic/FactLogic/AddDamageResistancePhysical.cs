142b2590a3c3c550ea07699aa9b075e012b57753
kaboissonneault <kaboissonneault@gmail.com>
2025-08-11 09:42:41 -0400

    Fix "AddDamageResistancePhysical" using "EnchantmentTotal" instead of "Enchantment" for "Magic" and "Alignment" DR bypass checks. "EnchantmentTotal" includes the "cost" of enchantments on the weapon, such as Flaming costing you a +1 for Paladin Weapon Bond. The issue is that in KM, many "enchantments" are traits like Composite or Thrown, which also add +1 to the total enchantment, despite being unrelated to how magic a weapon is. With this fix, Magic and Alignment DR checks only rely on the enhancement of the weapon, which matches the Material check, and matches the tabletop rules.

---
+++ kmbf/Patch/KM/UnitLogic/FactLogic/AddDamageResistancePhysical.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using Kingmaker.RuleSystem.Rules.Damage;
+using Kingmaker.UnitLogic.FactLogic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace kmbf.Patch.KM.UnitLogic.FactLogic
+{
+    // In KM, "EnhancementTotal" is a field used only for the purpose of checking whether the attack can bypass Damage Reduction with a "Enhancement" requirement
+    // In both KM and WotR, the "enchantment cost" of the enchantments on the weapon are added to the normal enhancement value
+    // This means that a +3 corrosive flaming sword gets a +1 from both corrosive and flaming for a total of +5, for bypassing DR
+    // This creates issues
+    // 1) This does not match tabletop
+    // 2) Enchantments are easily counted twice (they add to enhancement, and also have an enchantment cost to add to the total enhancement)
+    // 3) Random enchantments like "Composite" or "Thrown" add +1 (but not masterwork), when they have nothing to do with how magical the item is
+    //
+    // For this reason, this patch replaces the use of EnchantmentTotal with the regular Enchantment
+    // This is a different approach than the one used in WotR by the DragonFixes mod
+    [HarmonyPatch]
+    internal class AddDamageResistancePhysical_OnTrigger_Transpiler
+    {
+        static readonly MethodInfo Enchantment = AccessTools.PropertyGetter(typeof(PhysicalDamage), nameof(PhysicalDamage.Enchantment));
+        static readonly MethodInfo EnchantmentTotal = AccessTools.PropertyGetter(typeof(PhysicalDamage), nameof(PhysicalDamage.EnchantmentTotal));
+
+        [HarmonyPatch(typeof(AddDamageResistancePhysical), nameof(AddDamageResistancePhysical.Bypassed))]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> Bypassed_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return instructions.Select(i => i.Calls(EnchantmentTotal) ? new CodeInstruction(OpCodes.Callvirt, Enchantment) : i);
+        }
+
+        [HarmonyPatch(typeof(AddDamageResistancePhysical), nameof(AddDamageResistancePhysical.CheckBypassedByAlignment))]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> CheckBypassedByAlignment_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return instructions.Select(i => i.Calls(EnchantmentTotal) ? new CodeInstruction(OpCodes.Callvirt, Enchantment) : i);
+        }
+    }
+}
