0bae8cb28bfd0c1021b4f5465ec81f8ce682c83b
kaboissonneault <kaboissonneault@gmail.com>
2025-08-26 20:41:13 -0400

    Fixed null reference access in PathVisualizer Update

---
+++ kmbf/Patch/TB/PathVisualizer.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using Kingmaker.TurnBasedMode;
+using System.Reflection.Emit;
+using TurnBased.Controllers;
+
+namespace kmbf.Patch.TB
+{
+    [HarmonyPatch(typeof(PathVisualizer), nameof(PathVisualizer.Update))]
+    internal class PathVisualizer_Transpiler
+    {
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> Update_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var clearMethod = AccessTools.Method(typeof(PathVisualizer), nameof(PathVisualizer.Clear));
+            var get_CurrentTurn = AccessTools.PropertyGetter(typeof(CombatController), nameof(CombatController.CurrentTurn));
+
+            Label clearLabel = new Label();
+            List<CodeInstruction> current = instructions.ToList();
+            for (int index = 0; index < current.Count; index++)
+            {
+                CodeInstruction instruction = current[index];
+                if (instruction.Calls(clearMethod))
+                {
+                    clearLabel = current[index - 1].labels[0];
+                    break;
+                }
+            }
+
+            List<CodeInstruction> newInstructions = new List<CodeInstruction>();
+            bool firstCurrentTurn = true;
+            for (int index = 0; index < current.Count; index++)
+            {
+                CodeInstruction instruction = current[index];
+                if (firstCurrentTurn && instruction.Calls(get_CurrentTurn))
+                {
+                    // Add a null check before accessing Game.Instance.TurnBasedCombatController.CurrentTurn
+                    // If null, clear the path visualizer
+                    newInstructions.Add(instruction);
+                    newInstructions.AddRange(TranspilerUtility.UnityObjectConsumeNullCheckJump(clearLabel));
+                    // Reload the "current turn" (get Game, get CombatController, get Current Turn)
+                    newInstructions.Add(current[index - 2]);
+                    newInstructions.Add(current[index - 1]);
+                    newInstructions.Add(current[index]);
+
+                    firstCurrentTurn = false;
+                }
+                else
+                {
+                    newInstructions.Add(instruction);
+                }
+            }
+
+            return newInstructions;
+        }
+    }
+}
--- kmbf/Patch/TranspilerUtility.cs
+++ kmbf/Patch/TranspilerUtility.cs
@@ -9,7 +9,7 @@ namespace kmbf.Patch
     {
         static readonly MethodInfo UnityObjectEquals = AccessTools.Method(typeof(UnityEngine.Object), nameof(UnityEngine.Object.Equals));
 
-        // Expects a Unity.Object in the stack. If the reference is null, the function returns. If not, jumps to label
+        // Expects a Unity.Object in the stack. If the reference is null, the function returns. If not, jumps to label, object still in stack
         public static IEnumerable<CodeInstruction> UnityObjectNullCheckReturn(Label ifNotNull)
         {
             return [
@@ -19,6 +19,15 @@ namespace kmbf.Patch
                 , new CodeInstruction(OpCodes.Brfalse_S, ifNotNull)
                 , new CodeInstruction(OpCodes.Pop)
                 , new CodeInstruction(OpCodes.Ret)
+                ];
+        }
+
+        // Expects and consumes a Unity.Object in the stack. If the reference is null, the function returns. If not, jumps to label
+        public static IEnumerable<CodeInstruction> UnityObjectConsumeNullCheckJump(Label ifNull)
+        {
+            return [ new CodeInstruction(OpCodes.Ldnull)
+                , new CodeInstruction(OpCodes.Call, UnityObjectEquals)
+                , new CodeInstruction(OpCodes.Brtrue_S, ifNull)
                 ];
         }
     }
